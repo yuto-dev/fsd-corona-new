7d391c66db41a8a6f3cac4d2cac9490b76330d67
yuto-dev <directx178@gmail.com>
2020-06-04 08:06:16 +0800

    Menu created

--- ABIYYU_TAJ_MAHASIN_BAGINDO_TP058652.py
+++ ABIYYU_TAJ_MAHASIN_BAGINDO_TP058652.py
@@ -1,2 +1,54 @@
 #ABIYYU TAJ MAHASIN BAGINDO
 #TP058652
+
+def menu():
+
+    print("Welcome")
+    print("1. Register new patient")
+    print("2. Test patients")
+    print("3. Modify patient status")
+    print("4. Statistics")
+    print("5. Search")
+    print("6. Exit")
+
+    option = input("Choose option : ")
+
+    if option == "1":
+        registerPatient()
+    elif option == "2":
+        testPatient()
+    elif option == "3":
+        modifyPatient()
+    elif option == "4":
+        statPatient()
+    elif option == "5":
+        searchPatient()
+    elif option == "6":
+        print("Goodbye")
+    else:
+        print("Invalid option, please try again")        
+        menu()        
+
+
+
+def registerPatient():
+    print("a")
+    menu()
+    
+def testPatient():
+    print("b")
+    menu()
+
+def modifyPatient():
+    print("c")
+    menu()
+
+def statPatient():
+    print("d")
+    menu()
+
+def searchPatient():
+    print("e")    
+    menu()
+    
+menu()
